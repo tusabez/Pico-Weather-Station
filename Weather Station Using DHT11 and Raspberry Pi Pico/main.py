94393b62afc081e5b3070bc3598e8d3e52ff7d35
tusabez <68756279+tusabez@users.noreply.github.com>
2021-07-16 18:46:34 +0800

    Update main.py

--- Weather Station Using DHT11 and Raspberry Pi Pico/main.py
+++ Weather Station Using DHT11 and Raspberry Pi Pico/main.py
@@ -8,21 +8,23 @@ i2c = I2C(id=1,scl=Pin(27),sda=Pin(26),freq=100000)
 lcd = I2cLcd(i2c, 0x27, 2, 16)
 
 while True:
-    time.sleep(1)
-    pin = Pin(15, Pin.OUT, Pin.PULL_DOWN)
-    sensor = DHT11(pin)
-    t  = (sensor.temperature)
-    h = (sensor.humidity)
-    print("Temperature: {}".format(sensor.temperature))
-    print("Humidity: {}".format(sensor.humidity))
+     T,H = dht_sensor.read()
+    #Converted to Fahrenheit (FT) for LCD display
+    FT = T*1.8+32
+    if T is None:
+        print(" sensor error")
+    else:
+        print("{:3.1f}'C  {:3.1f}%".format(T,H))
+    #DHT22 not responsive if delay too short
+    time.sleep_ms(500)
     
-    time.sleep(1)
     lcd.clear()
     lcd.move_to(0,0)
-    lcd.putstr('Temp :')
-    lcd.move_to(7,0)
-    lcd.putstr(str(t)+" C")
+    lcd.putstr('Temp:')
+    lcd.move_to(10,0)
+    #If using Celsius, change (FT) to (T) and "F" to "C" on line below
+    lcd.putstr(str(round(FT, 1))+"F")
     lcd.move_to(0,1)
-    lcd.putstr('Humi :')
-    lcd.move_to(7,1)
-    lcd.putstr(str(h)+" %")
+    lcd.putstr('Humidity:')
+    lcd.move_to(10,1)
+    lcd.putstr(str(H)+"%")
